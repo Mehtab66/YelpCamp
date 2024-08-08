d7a3b2000e9c695d185947063c988f355203d2c9
Muhammad Mehtab AHmed <mehtabahmed7777777@gmail.com>
2024-08-08 22:17:28 +0500

    age added

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 const myname = "my name is muhammad mehtab ahmed";
+const age = 20;
 // MongoDB connection setup
 mongoose
   .connect("mongodb://127.0.0.1:27017/YELPCAMP", {
